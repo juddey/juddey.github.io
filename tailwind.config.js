5a5e7e2c332d9afa988367605b04512b5bc51891
Justin Lane <juddeylane@gmail.com>
2023-01-10 12:18:19 +1300

    Add Latest Work

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,30 @@ module.exports = {
     './*.markdown'
   ],
   theme: {
-    extend: {},
+    screens: {
+      screen: {"raw": "screen"},
+      print: {"raw": "print"},
+      xsm: "500px",
+      sm: "640px",
+      md: "811px",
+      lg: "1051px",
+      xl: "1280px"
+    },
+    extend: {
+      maxWidth: {
+        "letter": "66.40625rem",
+        "a4": "64.609375rem"
+      },
+      height: {
+        "letter": "85.9375rem",
+        "letter-col": "71.625rem",
+        "letter-col-full": "77.9375rem",
+        "a4": "91.350883rem",
+        "a4-col": "77.038383rem",
+        "a4-col-full": "83.350883rem",
+      },
+
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
